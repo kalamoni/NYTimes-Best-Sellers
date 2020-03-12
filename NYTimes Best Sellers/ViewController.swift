478a6a91d31fe3a867e98c164554651683206df4
Mohamed Salama <kalamoni007@gmail.com>
2020-03-12 22:42:44 +0100

    Initial Commit

---
+++ NYTimes Best Sellers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYTimes Best Sellers
+//
+//  Created by Omar Sherif on 3/12/20.
+//  Copyright © 2020 Mohamed Salama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NYTimes Best SellersTests/NYTimes_Best_SellersTests.swift
@@ -0,0 +1,34 @@
+//
+//  NYTimes_Best_SellersTests.swift
+//  NYTimes Best SellersTests
+//
+//  Created by Omar Sherif on 3/12/20.
+//  Copyright © 2020 Mohamed Salama. All rights reserved.
+//
+
+import XCTest
+@testable import NYTimes_Best_Sellers
+
+class NYTimes_Best_SellersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
